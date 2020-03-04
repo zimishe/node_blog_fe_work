7cf81005839d4df4c43d73a5f95a3f6a1035983f
Yevhen Shuliakivskyi <yevhen.shuliakivskyi@eleks.com>
2020-03-04 18:10:22 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [image, setImage] = useState("");
+
+  const getSignedrequest = async file => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8000/sign-s3?file-name=${file.name}&file-type=${file.type}`
+      );
+
+      uploadFile(response.data.signedRequest);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const uploadFile = signedRequest => {
+    axios.put(signedRequest);
+  };
+
+  const onFileSelected = e => {
+    const file = e.target.files[0];
+    console.log("file", file);
+
+    if (file) {
+      setImage(e.target.value);
+      getSignedrequest(file);
+    }
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .post(
+        "http://localhost:8000/articles",
+        {
+          title,
+          text
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAdC50IiwicGFzc3dvcmQiOiJ6aGVwYSIsImlhdCI6MTU4MzIyNjE3MX0.-NAioRK8A1aXR0Rws6KFW0Mv62qr0_AKCRaY2PDX0iU"
+          }
+        }
+      )
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={onSubmit} className="App">
+      <input
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        type="text"
+        placeholder="Article title"
+      />
+      <input
+        value={text}
+        onChange={e => setText(e.target.value)}
+        type="textarea"
+        placeholder="Article text"
+      />
+      <input
+        value={image}
+        onChange={onFileSelected}
+        type="file"
+        placeholder="Select article cover"
+      />
+      <button type="submit">Publish</button>
+    </form>
   );
-}
+};
 
 export default App;
